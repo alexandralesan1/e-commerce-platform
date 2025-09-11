453ecb58c7877f90f700f02dc607fea1a8a7d7cc
alexandralesan1 <alexandra.lesan@isa.utm.md>
2025-09-11 18:54:38 +0300

    Completed category entity

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/:path*",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
@@ -16,3 +20,5 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 };
+
+//publicRoutes: ["/api/:path*"]
